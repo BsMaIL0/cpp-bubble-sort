3e04b7503bb691f4835837b249de12fb1c0bef82
User Konstantin <koskakoska15@mail.ru>
2026-05-29 20:06:27 +0300

    Optimization

--- bubbleSort.hpp
+++ bubbleSort.hpp
@@ -2,11 +2,16 @@
 
 template <typename T>
 void bubbleSort(T A[], int size) {
-  for (int i = 0; i < size; ++i) {
-    for (int j = size - 1; j > 0; --j) {
+  for (int i = 0; i < size - 1; ++i) {
+    bool isSorted = true;
+    for (int j = size - 1; j > i; --j) {
       if (A[j] < A[j - 1]) {
         std::swap(A[j], A[j - 1]);
+        isSorted = false;
       }
+    }
+    if (isSorted) {
+      break;
     }
   }
 }
